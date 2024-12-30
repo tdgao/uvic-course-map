fb77e2ee7524f8e4c3dd82a850da9758fb778038
tdgao <mr.trumgao@gmail.com>
2024-12-30 12:45:30 -0800

    add levenshtein distance to ensure courseid is correct

--- uvic-course-map/src/lib/CourseView/CourseView.svelte
+++ uvic-course-map/src/lib/CourseView/CourseView.svelte
@@ -6,7 +6,68 @@
 	$: course = $activeCourse;
 
 	const courseIds = Object.keys(courses);
+
+	function sanitize(str: string) {
+		return str.replace(/\W+/g, '').toUpperCase();
+	}
+
+	// Use Levenshtein Distance :O
+	function levenshtein(a: string, b: string): number {
+		const an = a.length;
+		const bn = b.length;
+		if (an === 0) return bn;
+		if (bn === 0) return an;
+
+		const matrix: number[][] = [];
+
+		for (let i = 0; i <= bn; i++) {
+			matrix[i] = [i];
+		}
+		for (let j = 0; j <= an; j++) {
+			matrix[0][j] = j;
+		}
+
+		for (let i = 1; i <= bn; i++) {
+			for (let j = 1; j <= an; j++) {
+				if (b.charAt(i - 1) === a.charAt(j - 1)) {
+					matrix[i][j] = matrix[i - 1][j - 1];
+				} else {
+					matrix[i][j] = Math.min(
+						matrix[i - 1][j - 1] + 1,
+						matrix[i][j - 1] + 1,
+						matrix[i - 1][j] + 1
+					);
+				}
+			}
+		}
+
+		return matrix[bn][an];
+	}
+
+	function getClosestCourseId(input: string, courseIds: string[]): string {
+		const sanitizedInput = sanitize(input);
+
+		let bestId = courseIds[0];
+		let bestDistance = Infinity;
+
+		for (const cId of courseIds) {
+			const distance = levenshtein(sanitizedInput, sanitize(cId));
+			if (distance < bestDistance) {
+				bestDistance = distance;
+				bestId = cId;
+			}
+		}
+
+		return bestId; // the closest match
+	}
+
 	let searchCourseId = $graphCourseId;
+
+	const searchCourseOnChange = () => {
+		const matchedId = getClosestCourseId(searchCourseId, courseIds);
+		graphCourseId.set(matchedId);
+		searchCourseId = matchedId;
+	};
 </script>
 
 <div class="prose flex w-[500px] flex-col gap-4 p-4">
@@ -18,7 +79,7 @@
 			placeholder="CSC360"
 			class="input input-bordered w-full max-w-xs"
 			bind:value={searchCourseId}
-			onchange={() => ($graphCourseId = searchCourseId)}
+			onchange={searchCourseOnChange}
 		/>
 
 		<datalist id="all-course-ids">
