dffce9b71b9bb57e594f3afa36969f327feff850
tdgao <mr.trumgao@gmail.com>
2024-12-30 14:23:17 -0800

    add how to use modal

---
+++ uvic-course-map/src/lib/HowToUseModal.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	let modal: HTMLDialogElement;
+</script>
+
+<button class="btn btn-xs" on:click={() => modal.showModal()}>How to use?</button>
+
+<dialog id="my_modal_1" class="modal" bind:this={modal}>
+	<div class="modal-box">
+		<h3 class="text-lg font-bold">Hello!</h3>
+		<div class="flex flex-col gap-2 py-4">
+			<p>
+				This tool shows which courses depend on a specified prerequisite. It helps identify courses
+				you cannot take if the prerequisite is skipped.
+			</p>
+
+			<br />
+
+			<p><strong>How to Use</strong></p>
+			<ul>
+				<li>– Enter a course ID in the top left to generate a dependency map.</li>
+				<li>– Click any course node to view detailed information.</li>
+			</ul>
+		</div>
+
+		<div class="modal-action">
+			<form method="dialog">
+				<!-- if there is a button in form, it will close the modal -->
+				<button class="btn">Got it</button>
+			</form>
+		</div>
+	</div>
+</dialog>
--- uvic-course-map/src/routes/+page.svelte
+++ uvic-course-map/src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import MainContainer from '$lib/MainContainer/MainContainer.svelte';
 	import VisTreeGraph from '$lib/VisTreeGraph/VisTreeGraph.svelte';
 	import IslandContainer from '$lib/IslandContainer.svelte';
+	import HowToUseModal from '$lib/HowToUseModal.svelte';
 </script>
 
 <MainContainer>
@@ -12,11 +13,25 @@
 			<VisTreeGraph />
 		</div>
 
-		<div class="absolute left-0 top-0 m-4">
-			<GraphCourseSearch />
+		<div class="m- absolute left-0 top-0 w-full p-4">
+			<div class="flex flex-wrap items-start justify-between gap-2">
+				<GraphCourseSearch />
+				<div class="flex gap-2">
+					<!-- Open the modal using ID.showModal() method -->
+					<HowToUseModal />
+					<a
+						class="link font-mono"
+						href="https://github.com/tdgao/uvic-course-map"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Github
+					</a>
+				</div>
+			</div>
 		</div>
 
-		<div class="absolute bottom-0 left-0 m-4">
+		<div class="absolute bottom-0 left-0 p-4">
 			<IslandContainer>
 				<CourseView />
 			</IslandContainer>
